f7cbcba34632b8bad50ddb58857f2595ddd48089
Dmitriy Manzhura <d.manzhura@mmdsmart.com>
2021-10-23 11:36:43 +0300

    hw_15-10-21

--- 2.py
+++ 2.py
@@ -1,4 +1,9 @@
-list = [1, 2, 3, 4, 5, 6, 7]
-for i in range(len(list)):
-    list[i] *=list[i]
-print(list)
+def get_ext(filename):
+    a = filename.split('.')
+    if a[-1] != '' and len(a) != 1:
+        return a[-1]
+    return "value error"
+
+
+file_to_procces = input('please input filename')
+print(get_ext(file_to_procces))
