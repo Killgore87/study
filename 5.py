21f3b2c6dee8e215e1f7b352e3ced32acca97d28
Dmitriy Manzhura <d.manzhura@mmdsmart.com>
2021-10-23 13:50:27 +0300

    hw_15-10-21

--- 5.py
+++ 5.py
@@ -1,7 +1,19 @@
-list = []
-for _ in range(0, 100):
-    if _ == 0 or _ == 99:
-        list.append(1)
+def season_check(month):
+    """
+    please inout number of month
+    months 1,2,12 - winter
+    months 3,4,5 - spring
+    months 6,7,8 - summer
+    months 9,10,11 -fall
+    """
+    if 9 <= month <= 11:
+        return 'fall'
+    elif 3 <= month <= 5:
+        return 'spring'
+    elif 6 <= month <= 8:
+        return 'summer'
     else:
-        list.append(0)
-print(list)
+        return 'winter'
+
+
+print(season_check(int(input('input month'))))
