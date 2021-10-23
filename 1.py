0cbd287a8f7cc4c16191b49392bddc033b37e11c
Dmitriy Manzhura <d.manzhura@mmdsmart.com>
2021-10-23 11:17:11 +0300

    hw_15-10-21

--- 1.py
+++ 1.py
@@ -1,4 +1,15 @@
-numbers = ['first', 'second', '', 'abra', '', 'cadabra']
-numbers = [_ for _ in numbers if _ != '']
-print(numbers)
+numbers = input('please input numbers with delimeter space" " number>>').split()
+def list_calc(number, size):
+    result_list = []
+    result_number = 0
+    if int(size) != 1:
+        for i in range(1, int(size) + 1):
+            result_list.append(number * i)
+        for a in result_list:
+            result_number += int(a)
+        return result_number
+    else:
+        return number
+print(list_calc(number=numbers[0], size=numbers[1]))
+
 
