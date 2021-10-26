456bc28562e9afaa2a5fdbbcd32f3b0a34458f36
Killgore87 <dimitriusman@gmail.com>
2021-10-26 13:00:11 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,26 +1,13 @@
 def list_concater(first_text, second_text):
     result = []
-    if len(first_text) == len(second_text):
-        for i in range(len(first_text)):
-            result.append(first_text[i])
+    for i in range(len(first_text)):
+        result.append(first_text[i])
+        if i < len(second_text):
             result.append(second_text[i])
-        return result
-    else:
-        if len(first_text) > len(second_text):
-            for a in range(len(second_text)):
-                result.append(first_text[a])
-                result.append(second_text[a])
-            for b in range(len(first_text) - len(second_text)):
-                result.append(first_text[b])
-            return result
-        else:
-            for a in range(len(first_text)):
-                result.append(first_text[a])
-                result.append(second_text[a])
-            for b in range(len(second_text) - len(first_text)):
-                result.append(second_text[b])
-            return result
-
+            if i == (len(first_text) - 1) and i != (len(second_text) - 1):
+                for a in range(i, len(second_text)):
+                    result.append(second_text[a])
+    return result
 
 first_input = input('please input first text with delimeter space" " number>>').split()
 second_input = input('please input first text with delimeter space" " number>>').split()
